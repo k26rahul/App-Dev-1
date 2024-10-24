40ae26f703369ae28303300727a76d35dfe3fc0d
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2024-10-24 10:56:46 +0530

    render_template

--- week-4/flask/app.py
+++ week-4/flask/app.py
@@ -21,34 +21,12 @@ def quiz1():
 
 @app.route('/quiz/2')
 def quiz2():
-  return """<h1>quiz 2 is on <span style="color: green">3rd nov</span></h1>"""
+  return render_template('quiz2.html')
 
 
 @app.route('/html-test')
 def html():
-  return """<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Hello Vidu</title>
-</head>
-
-<body>
-  <h1 style="text-align: center; color: cyan">This is some big html doc</h1>
-
-  <table border='1'>
-    <tr>
-      <th>Day</th>
-    </tr>
-    <tr>
-      <td style="color: green">Thursday</td>
-    </tr>
-  </table>
-</body>
-
-</html>"""
+  return render_template('html-test.html')
 
 
 if __name__ == '__main__':
